6a7b09fc2e41faeab50c49764493aa54fedcb0b9
Diego Martinez <diego_two2@hotmail.com>
2025-05-23 10:54:50 -0500

    Subiendo el proyecto a github second commit

---
+++ Sistema-ventas-polleria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_ventas_polleria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
