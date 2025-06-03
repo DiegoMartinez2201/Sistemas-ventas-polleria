b7e8933787a70fd7f1377dfd0573d66733d7e43c
Soy Nelson <juareznelsonelias@gmail.com>
2025-06-03 09:14:49 -0500

    Frase para la plantilla de diseño

--- Sistema-ventas-polleria/obj/Debug/net8.0/Sistema-ventas-polleria.AssemblyInfo.cs
+++ Sistema-ventas-polleria/obj/Debug/net8.0/Sistema-ventas-polleria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema-ventas-polleria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+531590ad9557d277ac5d98706d37d6d51b0b6124")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f6c7ef63cc047e17d7bfa0a3ef92ef8d751301")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema-ventas-polleria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema-ventas-polleria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
