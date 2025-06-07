06d54c37e5f2f7a5b197bbe385c2fc55cf24edef
Diego Martinez <diego_two2@hotmail.com>
2025-06-07 09:21:57 -0500

    Resolviendo falta de diseño en vistas aun falta mejorar mas vistas

--- Sistema-ventas-polleria/Controllers/AdminController.cs
+++ Sistema-ventas-polleria/Controllers/AdminController.cs
@@ -3,13 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Sistema_ventas_polleria.Controllers
 {
-    [Authorize]
+    /*[Authorize]*/
     public class AdministradorController : Controller
     {
         public IActionResult Dashboard()
         {
-            if (User.FindFirst("idTipoCliente")?.Value != "1")
-                return Unauthorized();
+            /*if (User.FindFirst("idTipoCliente")?.Value != "1")
+                return Unauthorized();*/
+
 
             return View();
         }
--- Sistema-ventas-polleria/Controllers/HomeController.cs
+++ Sistema-ventas-polleria/Controllers/HomeController.cs
@@ -28,9 +28,6 @@ namespace Sistema_ventas_polleria.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public IActionResult Dashboard()
-        {
-            return View();
-        }
+       
     }
 }
