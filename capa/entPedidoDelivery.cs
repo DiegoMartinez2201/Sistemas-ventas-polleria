5b562af8d844bdc35caa79b18a344b57620845a0
Junior <giancarlosvl55@gmail.com>
2025-07-11 03:21:37 -0500

    Se ha creado la clase entDelivery con sus propiedades

---
+++ capa/entPedidoDelivery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capa
+{
+    public class entPedidoDelivery
+    {
+        public int IdPedido { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Estado { get; set; }
+
+        
+
+
+
+
+    }
+}
