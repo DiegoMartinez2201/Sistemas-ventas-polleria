94c5e53658c5aaa2d51b683962216f3c2298a44c
Junior <giancarlosvl55@gmail.com>
2025-07-11 03:26:34 -0500

    Se ha creado la clase logPedidoDelivery, ademas se comento dos metodos para la funcionalidad que se pueda añdir mas adelante, falta mejorar

---
+++ capaLogica/logPedidoDelivery.cs
@@ -0,0 +1,38 @@
+﻿using capa;
+using capaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaLogica
+{
+    public class logPedidoDelivery
+    {
+         private static readonly logPedidoDelivery _instancia = new logPedidoDelivery();
+         public static logPedidoDelivery Instancia { get { return _instancia; } }
+
+
+
+        // Método para listar pedidos por cliente
+        /*public List<entPedidoDelivery> ListarPedidosPorCliente(int idCliente)
+        {
+            return datPedidoDelivery.Instancia.ListarPedidosPorCliente(idCliente);
+        }*/
+        // Método para actualizar el estado de un pedido
+
+
+        /*[HttpPost]
+        public IActionResult ActualizarEstado(int idPedido, string nuevoEstado)
+        {
+            // Llama a la lógica para actualizar el estado del pedido
+            logPedidoDelivery.Instancia.ActualizarEstadoPedido(idPedido, nuevoEstado);
+
+            // Redirige de nuevo a la lista de pedidos
+            return RedirectToAction("ListarPedido");
+        }
+        */
+    }
+
+}
